6817e3f199a2ac5a92c3134ea2e26daca87d7310
PenGuinMike <104021074@live.asia.edu.tw>
2016-12-12 15:58:02 +0800

    savesa

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,10 +1,31 @@
 package ex;
+
+import java.util.Scanner;
+
 /*
  * Date: 2016/12/12
  * Author: 105021***  ???
  */
 public class ex01 {
 	public static void main(String[] args) {
-
+		Scanner scn = new Scanner(System.in);
+		int dataSize =scn.nextInt();
+		if(dataSize>10||dataSize<1){
+			System.out.println("erro");
+		}
+		int data1 []=new int [3];
+		for(int i=0;i<3;i++){
+			data1[i]=scn.nextInt();
+		}int data2 []=new int [data1[2]];
+		for(int i=0;i<data1[2];i++){
+			data2[i]=scn.nextInt();
+		}
+		for(int i=data1[1];i>0;i--){
+			for(int j=0;j<data1[1];j++){
+				if(){
+					
+				}
+			}
+		}
 	}
 }
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -3,8 +3,19 @@ package ex;
  * Date: 2016/12/12
  * Author: 105021***  ???
  */
+import java.util.*;
+import java.util.*;
 public class ex02 {
 	public static void main(String[] args) {
-
+		Scanner scn = new Scanner(System.in);
+		System.out.println("請輸入數字: ");
+		String str = scn.nextLine();
+		String a[]=(str.split(" "));
+		int data[]=new int [a.length];
+		for(int i=0;i<a.length;i++){
+			data[i]=Integer.valueOf(a[i]);
+			System.out.print(data[i]+" ");
+		}
+		
 	}
 }
