f19dc61fde42bf1080d2c49e2be6ac8ef47c5d16
PenGuinMike <104021074@live.asia.edu.tw>
2016-12-13 18:14:54 +0800

    凱薩加密法

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -3,8 +3,33 @@ package hw;
  * Date: 2016/12/12
  * Author: 105021***  ???
  */
+import java.util.*;
 public class hw01 {
 	public static void main(String[] args) {
-
+		Scanner scn =new Scanner(System.in);
+		System.out.println("請輸入文字: ");
+		String str = scn.nextLine();
+		System.out.println("請輸入要加的數字: ");
+		int a = scn.nextInt();
+		char data[]=str.toCharArray();
+		for(int i=0;i<data.length;i++){
+			if(data[i]>64&&data[i]<91){
+				if((int)data[i]+a>90){
+					data[i]=(char)((int)data[i]+a-26);
+				}else{
+					data[i]=(char)((int)data[i]+a);
+				}
+			}else if(data[i]>96&&data[i]<123){
+				if((int)data[i]+a>122){
+					data[i]=(char)((int)data[i]+a-26);
+				}else{
+					data[i]=(char)((int)data[i]+a);
+				}
+			}else{
+				data[i]=data[i];
+			}
+		}
+		String text = new String(data);
+		System.out.println(" Text: "+text);
 	}
 }
