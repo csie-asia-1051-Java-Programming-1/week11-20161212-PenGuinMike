8cd279271888f50e4539a30d620385c3820cf171
PenGuinMike <104021074@live.asia.edu.tw>
2016-12-13 20:12:49 +0800

    老闆買禮物 -存檔

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -14,18 +14,34 @@ public class ex01 {
 			System.out.println("erro");
 		}
 		int data1 []=new int [3];
+		
 		for(int i=0;i<3;i++){
 			data1[i]=scn.nextInt();
-		}int data2 []=new int [data1[2]];
+		}
+		int data2 []=new int [data1[2]];
+		
 		for(int i=0;i<data1[2];i++){
 			data2[i]=scn.nextInt();
 		}
-		for(int i=data1[1];i>0;i--){
-			for(int j=0;j<data1[1];j++){
-				if(){
-					
+		
+		for(int i=0;i<data2.length-1;i++){
+			for(int j=0;j<data2.length-1;j++){
+				if(data2[j+1]>data2[j]){
+					int change=data2[j+1];
+					data2[j+1]=data2[j];
+					data2[j]=change;
 				}
 			}
 		}
+		int sum=0;
+		for(int i=0;i<data1[2];i++){
+			sum+=data2[i];
+		}if (sum<data1[1]){
+			System.out.println(sum);
+		}else{
+			System.out.println("impossible");
+		}
+
 	}
+
 }
